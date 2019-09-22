ed475996ea207266df0ed21fb6d59f4277498a4d
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-22 16:34:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,14 +4,14 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {name:"Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age:"54"},
-      :hero => {name: "Romeo", age: "15",status:"alive"},
+      :hero => {name: "Romeo", age: "15", status:"alive"},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {name:"Lord Capulet", age: "50",},
-      :matriarch => {name: "Lady Capulet", age: "51",},
-      :heroine => {name: "Juliet" age:"15", status:"alive"},
+      :patriarch => {name:"Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age:"15", status:"alive"},
       :heroine_friends => []
       }
 
